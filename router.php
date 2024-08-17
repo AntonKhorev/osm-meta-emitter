3c00b39fb55709079d3553918d89ddfa7482c8d0
Anton Khorev <tony29@yandex.ru>
2024-08-17 18:57:01 +0300

    Init

---
+++ router.php
@@ -0,0 +1,18 @@
+<?php
+
+if (php_sapi_name() == 'cli-server') {
+	$root = "/";
+} else {
+	if (preg_match('{(.*/)[^/]*\.php$}', $_SERVER['SCRIPT_NAME'], $matches)) {
+		$root = $matches[1];
+	} else {
+		$root = "/";
+	}
+}
+
+if (substr($_SERVER['REQUEST_URI'], 0, strlen($root)) == $root) {
+	$request = substr($_SERVER['REQUEST_URI'], strlen($root));
+}
+
+echo "<div>Root: " . htmlspecialchars($root) . "</div>\n";
+echo "<div>Request: " . htmlspecialchars($request) . "</div>\n";
