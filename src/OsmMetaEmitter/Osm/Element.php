b8645192d5b9ab7e975e5b6702fc3baaf989b501
Anton Khorev <tony29@yandex.ru>
2024-10-01 05:09:06 +0300

    Show banking amenities at zoom 18

--- src/OsmMetaEmitter/Osm/Element.php
+++ src/OsmMetaEmitter/Osm/Element.php
@@ -3,7 +3,10 @@
 class Element {
 	public const DEFAULT_MAX_ZOOM = 16;
 	public const AMENITY_MAX_ZOOM = 18;
-	public const AMENITY_MAX_ZOOM_TAG_VALUES = ["biergarten", "cafe", "fast_food", "food_court", "ice_cream", "pub", "restaurant"]; // https://github.com/gravitystorm/openstreetmap-carto/blob/23b1cfa7284ac91bb78390fa4cb7f1c2c6350b92/style/amenity-points.mss#L84
+	public const AMENITY_MAX_ZOOM_TAG_VALUES = [
+		"atm", "bureau_de_change", "bank", // https://github.com/gravitystorm/openstreetmap-carto/blob/23b1cfa7284ac91bb78390fa4cb7f1c2c6350b92/style/amenity-points.mss#L66
+		"biergarten", "cafe", "fast_food", "food_court", "ice_cream", "pub", "restaurant" // https://github.com/gravitystorm/openstreetmap-carto/blob/23b1cfa7284ac91bb78390fa4cb7f1c2c6350b92/style/amenity-points.mss#L84
+	];
 	public bool $visible = true;
 
 	function __construct(
